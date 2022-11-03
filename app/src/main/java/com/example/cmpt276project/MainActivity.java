2cb0bb961264dd856f763696ba3950fc56167ddf
ska287 <ska287@sfu.ca>
2022-11-03 13:49:27 -0700

    added gamelist
    added games to game catories

--- app/src/main/java/com/example/cmpt276project/MainActivity.java
+++ app/src/main/java/com/example/cmpt276project/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.cmpt276project;
 
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +20,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        getSupportActionBar().hide();
 
-
-        setTitle("Debugger");
-
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
         new Handler().postDelayed(new Runnable() {
             //this is the coolest thing I ever done
             @Override
