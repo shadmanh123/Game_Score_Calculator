3dcc7777c70933193d139563729f05c14b6a6ae9
shadmanhossain <shadmanh@sfu.ca>
2022-11-05 12:53:39 -0700

    Created functions for adding play
    Function to create play was made

--- app/src/main/java/com/example/cmpt276project/AddPlayGameHistoryActivity.java
+++ app/src/main/java/com/example/cmpt276project/AddPlayGameHistoryActivity.java
@@ -4,18 +4,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextWatcher;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AddPlayGameHistoryActivity extends AppCompatActivity {
-    EditText etTotalPlayers = findViewById(R.id.etTotalPlayers);
-    EditText etTotalScore = findViewById(R.id.etTotalScore);
-    int totalPlayers;
-    int totalScore;
+import com.example.cmpt276project.model.Play;
 
+public class AddPlayGameHistoryActivity extends AppCompatActivity {
+    Button enter = findViewById(R.id.btnEnter);
     public static Intent makeIntent(Context context){
         return new Intent(context, AddPlayGameHistoryActivity.class);
     }
@@ -26,25 +25,56 @@ public class AddPlayGameHistoryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_edit_game_history);
         getNumberOfPlayers();
         getTotalScore();
+        //enter.setOnClickListener(v -> createPlay(getNumberOfPlayers(),getTotalScore()));
     }
 
-
-    private void getNumberOfPlayers() {
+    private int getNumberOfPlayers() {
+        EditText etTotalPlayers = findViewById(R.id.etTotalPlayers);
         String players = etTotalPlayers.getText().toString();
-        totalPlayers = Integer.parseInt(players);
-        validateNumberOfPlayers();
+        int totalPlayers = Integer.parseInt(players);
+        while(!validateNumberOfPlayers(totalPlayers)){
+            players = etTotalPlayers.getText().toString();
+            totalPlayers = Integer.parseInt(players);
+        }
+        return totalPlayers;
     }
 
-    private void validateNumberOfPlayers() {
-        if(totalPlayers <= 0){
-            Toast.makeText(this, "Total Players must be greater than 0",
+    private boolean validateNumberOfPlayers(int numberOfPlayers) {
+        if(numberOfPlayers <= 0){
+            Toast.makeText(this, "Total Number of Players must be greater than 0",
                     Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else{
+            return true;
         }
     }
 
-    private void getTotalScore() {
+    private int getTotalScore() {
+        EditText etTotalScore = findViewById(R.id.etTotalScore);
         String score = etTotalScore.getText().toString();
-        totalScore = Integer.parseInt(score);
+        int totalScore = Integer.parseInt(score);
+        while(!validateTotalScore(totalScore)){
+            score = etTotalScore.getText().toString();
+            totalScore = Integer.parseInt(score);
+        }
+        return totalScore;
+    }
+
+    private boolean validateTotalScore(int totalScore) {
+        if(totalScore < 0){
+            Toast.makeText(this, "Combined Score must be at least 0",
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private void createPlay(int numberOfPlayers, int totalScore) {
+        Play game;
+
     }
 
 }
--- app/src/main/java/com/example/cmpt276project/CreditsActivity.java
+++ app/src/main/java/com/example/cmpt276project/CreditsActivity.java
@@ -1,11 +1,15 @@
 package com.example.cmpt276project;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class CreditsActivity extends AppCompatActivity {
-
+    public static Intent makeIntent(Context context){
+        return new Intent(context, CreditsActivity.class);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
