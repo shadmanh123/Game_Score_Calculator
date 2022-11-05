494a11e8419e3b55b2cd7db6dc1f46482d6a9fc0
Serena <sad11@sfu.ca>
2022-11-05 01:07:36 -0700

    got back button working

--- app/src/main/java/com/example/cmpt276project/AddEditGameCategoryActivity.java
+++ app/src/main/java/com/example/cmpt276project/AddEditGameCategoryActivity.java
@@ -1,10 +1,12 @@
 package com.example.cmpt276project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -53,7 +55,12 @@ public class AddEditGameCategoryActivity extends AppCompatActivity {
 
     private void onBackClick() {
         //has a pop up saying that things are not saved
-        finish();
+        FragmentManager manager = getSupportFragmentManager();
+        BackPopUpGCFragment dialog = new BackPopUpGCFragment();
+        dialog.show(manager, "message");
+
+        Log.i("TAG", "just show");
+//        finish();
     }
 
 
