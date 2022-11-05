e4880a57d690eb9ded2c5fee752cb8a90b477aa9
tiang01 <tiang01@students.cs.ubc.ca>
2022-11-05 11:24:45 -0700

    Setting up table for list of plays

--- app/src/main/java/com/example/cmpt276project/GameHistoryActivity.java
+++ app/src/main/java/com/example/cmpt276project/GameHistoryActivity.java
@@ -1,6 +1,9 @@
 package com.example.cmpt276project;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +13,25 @@ public class GameHistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_history);
+        populateButtons();
     }
+
+    // TODO: Change variable to something else
+    final int gameManagerSize = 10;
+    final int COLUMN_SIZE = 4;
+    private void populateButtons() {
+        TableLayout table = findViewById(R.id.tableForHistory);
+        for (int row = 1; row < gameManagerSize; row++) {
+            TableRow tableRow = new TableRow(this);
+            table.addView(tableRow);
+            for(int col  = 0; col < COLUMN_SIZE; col++) {
+                TextView tv = new TextView(this);
+                tableRow.addView(tv);
+
+
+            }
+        }
+    }
+
+
 }
