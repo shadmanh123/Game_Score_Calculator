b63fb4388260388d969cf9ee9a9d7079a91d0bcf
shadmanhossain <shadmanh@sfu.ca>
2022-11-05 18:37:39 -0700

    Updated Credits
    Updated information for CreditsActivity.java

--- app/src/main/java/com/example/cmpt276project/CreditsActivity.java
+++ app/src/main/java/com/example/cmpt276project/CreditsActivity.java
@@ -3,6 +3,8 @@ package com.example.cmpt276project;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,5 +16,11 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+        setupHyperlink();
+    }
+    private void setupHyperlink() {
+        // set hyperlink
+        TextView linkTextView = findViewById(R.id.tvCredits);
+        linkTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
