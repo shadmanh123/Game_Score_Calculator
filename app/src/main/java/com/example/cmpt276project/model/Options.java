73e5459919f9824d61132c0d9c9089a5db2594d4
Serena <sad11@sfu.ca>
2022-11-15 09:31:23 -0800

    Created options class

---
+++ app/src/main/java/com/example/cmpt276project/model/Options.java
@@ -0,0 +1,29 @@
+package com.example.cmpt276project.model;
+
+public class Options {
+    //choosing the play difficulty level and the play theme
+    private String difficulty;
+    private String theme;
+
+    //going to have default has normal difficulty and sea theme - just since that was our original
+    public Options(String difficulty, String theme) {
+        this.difficulty = difficulty;
+        this.theme = theme;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+}
