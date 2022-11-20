2a5654a769f1bf714143aef0588c4fef393c9adf
ska287 <ska287@sfu.ca>
2022-11-19 16:17:43 -0800

    added sound

--- app/src/main/java/com/example/cmpt276project/AchievementAnimationActivity.java
+++ app/src/main/java/com/example/cmpt276project/AchievementAnimationActivity.java
@@ -3,19 +3,50 @@ package com.example.cmpt276project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
+import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 
 public class AchievementAnimationActivity extends AppCompatActivity {
-
+    private SoundPool soundPool;
+    private int sound1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_achievement_animation);
 
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            AudioAttributes audioAttributes = new AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ASSISTANCE_SONIFICATION)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build();
+            soundPool = new SoundPool.Builder()
+                    .setMaxStreams(6)
+                    .setAudioAttributes(audioAttributes)
+                    .build();
+        }else{
+            soundPool = new SoundPool(6, AudioManager.STREAM_MUSIC,0);
+        }
+
+        sound1 = soundPool.load(this, R.raw.sound1, 1);
+
+
+
+
+
+
+
+
+
+
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                soundPool.play(sound1, 1, 1, 0 , 0, 1);
                 startActivity(new Intent(AchievementAnimationActivity.this, PlayActivity.class));
                 finish();
             }
